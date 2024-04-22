ea8d441c536bc80f4bb9a56940ed8511b27fb661
MeliLiz <melifernandez@ciencias.unam.mx>
2024-04-22 10:00:43 -0600

    ruta a product

--- src/app/module/layout/app-layout/app-layout.routing.ts
+++ src/app/module/layout/app-layout/app-layout.routing.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { CategoryComponent } from '../../product/component/category/category.component';
+import { ProductComponent } from '../../product/component/product/product.component';
 import { RegisterComponent } from '../../authentication/register/register.component';
 import { LoginComponent } from '../../authentication/login/login.component';
 import { SecuredComponent } from '../../authentication/secured/secured.component';
@@ -11,5 +12,6 @@ export const AppLayoutRoutes: Routes = [
     { path: 'category', component: CategoryComponent },
     // {path: 'register', component: RegisterComponent},
     // {path: 'login', component: LoginComponent},
-    { path: 'secured', component: SecuredComponent, canActivate: [authenticationGuard] }
+    { path: 'secured', component: SecuredComponent, canActivate: [authenticationGuard] },
+    { path: 'product', component: ProductComponent }
 ];
